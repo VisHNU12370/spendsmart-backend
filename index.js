6d92dfe2170657c2c97d69b455a2e134574dc4cb
Vishnu <vishnukaspe19@gmail.com>
2026-06-03 02:53:56 +0530

    Change notification time to 2:55 AM

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ mongoose
   .catch((err) => console.error("❌ MongoDB Error:", err));
 
 // Cron Job — 11:50 PM India = 6:20 PM UTC
-cron.schedule("19 9 * * *", () => {
+cron.schedule("25 21 * * *", () => {
   console.log("🔔 Running nightly notification job...");
   nightNotification.sendNightlyReport();
 });
